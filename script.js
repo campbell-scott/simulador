1e5b5fa8c0e2deee28b425914c05ac01fdbd1ac8
campbell-scott <scott99campbell@gmail.com>
2022-07-26 13:05:54 -0300

    tercer entrga

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-function promedio(){
-    let nota1 = parseInt(prompt("Ingrese la primer nota"))
-    let nota2 = parseInt(prompt("Ingrese la segunda nota"))
-    let nota3 = parseInt(prompt("Ingrese la tercer nota"))
-    return (nota1+nota2+nota3)/3
+function promedio(x){
+    const total = x.reduce((a, b) => a + b, 0)
+    return total/(x.length)
 }
 
 let cantAlumnos = parseInt(prompt("Ingrese la cantidad de alumnos"))
+let numeros = []
+let entrada
 
 for (let i = 1; i <= cantAlumnos; i++) {
     let nombre = prompt("Ingrese el nombre del alumno")
-    let promNotas = promedio()
+    do{
+        entrada = parseInt(prompt("ingrese notas del alumno, para salir ingrese '0'"))
+        if (entrada != 0) {
+            numeros.push(entrada)
+        }
+     } while (entrada != 0);
+     let promNotas = promedio(numeros)
     console.log("La nota final de "+nombre+" es: ",promNotas)
 }
 
